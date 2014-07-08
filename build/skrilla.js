4308a96c2f7e5952983c7b5a31c0fceb7eef6ed4
Josh Rowley <joshua.f.rowley@gmail.com>
2014-07-08 16:26:57 -0400

    add amd and common js support

--- build/skrilla.js
+++ build/skrilla.js
@@ -1,95 +1,105 @@
-var Skrilla;
+(function(window) {
+  var Skrilla;
+  Skrilla = (function() {
+    var concat_styles, set_keyframes;
 
-Skrilla = (function() {
-  var concat_styles, set_keyframes;
+    Skrilla.prototype.start = 0;
 
-  Skrilla.prototype.start = 0;
+    Skrilla.prototype.end = 0;
 
-  Skrilla.prototype.end = 0;
+    Skrilla.prototype.el = '';
 
-  Skrilla.prototype.el = '';
+    Skrilla.prototype.keyframes = {};
 
-  Skrilla.prototype.keyframes = {};
-
-  function Skrilla(args) {
-    var k, v;
-    for (k in args) {
-      v = args[k];
-      this[k] = v;
-    }
-  }
-
-  Skrilla.prototype.beforeInit = function() {};
-
-  Skrilla.prototype.afterInit = function() {};
-
-  Skrilla.prototype.init = function() {
-    this.beforeInit();
-    set_keyframes.call(this);
-    this.set_arrow_listeners();
-    return this.afterInit();
-  };
-
-  Skrilla.prototype.animate_to = function(pos, opts) {
-    var s;
-    if (!is_mobile) {
-      s = skrollr.get();
-      if (s) {
-        return s.animateTo(this.percent_to_absolute(pos), opts);
+    function Skrilla(args) {
+      var k, v;
+      for (k in args) {
+        v = args[k];
+        this[k] = v;
       }
     }
-  };
 
-  Skrilla.prototype.percent_to_absolute = function(percent) {
-    return ((percent * 0.01) * (this.end - this.start)) + this.start;
-  };
+    Skrilla.prototype.beforeInit = function() {};
 
-  Skrilla.prototype.set_arrow_listeners = function() {
-    if (this.arrow) {
-      return $(this.arrow).on('click', (function(_this) {
-        return function() {
-          ga('send', 'event', _this.arrow, 'click');
-          return _this.animate_to(_this.arrow_target, {
-            duration: _this.arrow_duration
-          });
-        };
-      })(this));
-    }
-  };
+    Skrilla.prototype.afterInit = function() {};
 
-  set_keyframes = function() {
-    var abs, el, frames, percent, styles, _ref, _results;
-    _ref = this.keyframes;
-    _results = [];
-    for (el in _ref) {
-      frames = _ref[el];
-      _results.push((function() {
-        var _results1;
-        _results1 = [];
-        for (percent in frames) {
-          styles = frames[percent];
-          abs = this.percent_to_absolute(percent);
-          if (el === 'this') {
-            el = '';
-          }
-          _results1.push($("" + this.el + " " + el).attr("data-" + abs, concat_styles(styles)));
+    Skrilla.prototype.init = function() {
+      this.beforeInit();
+      set_keyframes.call(this);
+      this.set_arrow_listeners();
+      return this.afterInit();
+    };
+
+    Skrilla.prototype.animate_to = function(pos, opts) {
+      var s;
+      if (!is_mobile) {
+        s = skrollr.get();
+        if (s) {
+          return s.animateTo(this.percent_to_absolute(pos), opts);
         }
-        return _results1;
-      }).call(this));
-    }
-    return _results;
-  };
+      }
+    };
 
-  concat_styles = function(styles) {
-    var prop, result, val;
-    result = '';
-    for (prop in styles) {
-      val = styles[prop];
-      result += "" + prop + ":" + val + ";";
-    }
-    return result;
-  };
+    Skrilla.prototype.percent_to_absolute = function(percent) {
+      return ((percent * 0.01) * (this.end - this.start)) + this.start;
+    };
 
-  return Skrilla;
+    Skrilla.prototype.set_arrow_listeners = function() {
+      if (this.arrow) {
+        return $(this.arrow).on('click', (function(_this) {
+          return function() {
+            ga('send', 'event', _this.arrow, 'click');
+            return _this.animate_to(_this.arrow_target, {
+              duration: _this.arrow_duration
+            });
+          };
+        })(this));
+      }
+    };
 
-})();
+    set_keyframes = function() {
+      var abs, el, frames, percent, styles, _ref, _results;
+      _ref = this.keyframes;
+      _results = [];
+      for (el in _ref) {
+        frames = _ref[el];
+        _results.push((function() {
+          var _results1;
+          _results1 = [];
+          for (percent in frames) {
+            styles = frames[percent];
+            abs = this.percent_to_absolute(percent);
+            if (el === 'this') {
+              el = '';
+            }
+            _results1.push($("" + this.el + " " + el).attr("data-" + abs, concat_styles(styles)));
+          }
+          return _results1;
+        }).call(this));
+      }
+      return _results;
+    };
+
+    concat_styles = function(styles) {
+      var prop, result, val;
+      result = '';
+      for (prop in styles) {
+        val = styles[prop];
+        result += "" + prop + ":" + val + ";";
+      }
+      return result;
+    };
+
+    return Skrilla;
+
+  })();
+  if (typeof define === 'function' && define.amd) {
+    return define(function() {
+      return Skrilla;
+    });
+  } else if (typeof module !== 'undefined' && module.exports) {
+    return module.exports = Skrilla;
+  } else {
+    return window.Skrilla = Skrilla;
+  }
+})(window);
--- src/skrilla.coffee
+++ src/skrilla.coffee
@@ -1,49 +1,59 @@
-class Skrilla
-  start: 0
-  end: 0
-  el: ''
-  keyframes: {}
+((window) ->
+  class Skrilla
+    start: 0
+    end: 0
+    el: ''
+    keyframes: {}
 
-  constructor: (args) ->
-    @[k] = v for k, v of args
+    constructor: (args) ->
+      @[k] = v for k, v of args
 
-  beforeInit: ->
+    beforeInit: ->
 
-  afterInit: ->
+    afterInit: ->
 
-  init: ->
-    @beforeInit()
-    set_keyframes.call(@)
-    @set_arrow_listeners()
-    @afterInit()
+    init: ->
+      @beforeInit()
+      set_keyframes.call(@)
+      @set_arrow_listeners()
+      @afterInit()
 
-  animate_to: (pos, opts) ->
-    if not is_mobile
-      s = skrollr.get()
-      if s then s.animateTo(@percent_to_absolute(pos), opts)
+    animate_to: (pos, opts) ->
+      if not is_mobile
+        s = skrollr.get()
+        if s then s.animateTo(@percent_to_absolute(pos), opts)
 
-  percent_to_absolute: (percent) ->
-    ((percent * 0.01) * (@end - @start)) + @start
+    percent_to_absolute: (percent) ->
+      ((percent * 0.01) * (@end - @start)) + @start
 
-  set_arrow_listeners: ->
-    if @arrow
-      $(@arrow).on 'click', =>
-        ga('send', 'event', @arrow, 'click')
-        @animate_to(@arrow_target, {duration: @arrow_duration})
+    set_arrow_listeners: ->
+      if @arrow
+        $(@arrow).on 'click', =>
+          ga('send', 'event', @arrow, 'click')
+          @animate_to(@arrow_target, {duration: @arrow_duration})
 
-  #
-  # private
-  #
+    #
+    # private
+    #
 
-  set_keyframes = ->
-    for el, frames of @keyframes
-      for percent, styles of frames
-        abs = @percent_to_absolute(percent)
-        if el == 'this' then el = ''
-        $("#{@el} #{el}").attr("data-#{abs}", concat_styles(styles))
+    set_keyframes = ->
+      for el, frames of @keyframes
+        for percent, styles of frames
+          abs = @percent_to_absolute(percent)
+          if el == 'this' then el = ''
+          $("#{@el} #{el}").attr("data-#{abs}", concat_styles(styles))
 
-  concat_styles = (styles) ->
-    result = ''
-    for prop, val of styles
-      result += "#{prop}:#{val};"
-    result
+    concat_styles = (styles) ->
+      result = ''
+      for prop, val of styles
+        result += "#{prop}:#{val};"
+      result
+
+  if (typeof define == 'function' && define.amd)
+    define -> Skrilla
+  else if (typeof module != 'undefined' && module.exports)
+    module.exports = Skrilla
+  else
+    window.Skrilla = Skrilla
+
+)(window)
